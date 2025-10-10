1db2e18b5fc28d31756a70c961110b8570e34fe3
EzequielL99 <ezequiel.lopez.vallejos@gmail.com>
2025-10-09 23:33:05 -0300

    Instalacion Google Fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <Button className="font-montserrat">Hola mundo</Button>
+      <Button className="font-montserrat font-thin">Hola mundo</Button>
+      <Button className="font-montserrat font-bold">Hola mundo</Button>
+    </>
   )
 }
